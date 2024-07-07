b8da515b44b5a005138f2f9833654681b7148240
randeer <randeerlalanga92@gmail.com>
2024-07-07 10:37:52 +0000

    final

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 from pathlib import Path
 import shutil
 import yaml
+import subprocess
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
@@ -68,6 +69,62 @@ async def insert_into_database(user_info: UserInfo):
         if conn:
             conn.close()
             logger.info("Database connection closed")
+
+async def create_wordpress_db(user_info: UserInfo):
+    conn = None
+    try:
+        logger.info(f"Attempting to connect to database with config: {db_config}")
+        conn = await aiomysql.connect(**db_config)
+        logger.info("Database connection established")
+        
+        async with conn.cursor() as cursor:
+            # Create database
+            db_name = f"user{user_info.userID}"
+            sql_create_db = f"CREATE DATABASE IF NOT EXISTS {db_name}"
+            await cursor.execute(sql_create_db)
+            
+            # Create user and grant privileges
+            db_username = f"user{user_info.userID}"
+            db_password = f"password{user_info.userID}"
+            sql_create_user = f"CREATE USER IF NOT EXISTS '{db_username}'@'%' IDENTIFIED BY '{db_password}'"
+            await cursor.execute(sql_create_user)
+            
+            sql_grant_privileges = f"GRANT ALL PRIVILEGES ON {db_name}.* TO '{db_username}'@'%'"
+            await cursor.execute(sql_grant_privileges)
+            
+            await cursor.execute("FLUSH PRIVILEGES")
+            
+            await conn.commit()
+            
+            return {
+                "message": f"WordPress database and user created successfully.",
+                "db_name": db_name,
+                "db_username": db_username,
+                "db_password": db_password,
+                "db_hostname": "docker.lala-1992.xyz"
+            }
+    except aiomysql.Error as e:
+        logger.error(f"Database error: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Database error: {str(e)}")
+    except Exception as e:
+        logger.error(f"Unexpected error: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Unexpected error: {str(e)}")
+    finally:
+        if conn:
+            conn.close()
+            logger.info("Database connection closed")
+
+@app.post("/push_data_db/create_wordpress_db/")
+async def create_wordpress_db_endpoint(user_info: UserInfo):
+    try:
+        result = await create_wordpress_db(user_info)
+        return result
+    except HTTPException as he:
+        logger.error(f"HTTP exception in create_wordpress_db: {str(he)}")
+        raise he
+    except Exception as e:
+        logger.error(f"Unexpected error in create_wordpress_db: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to create WordPress database: {str(e)}")
 
 @app.post("/create_folder/")
 async def create_folder(user_info: UserInfo):
@@ -163,6 +220,8 @@ async def update_template_files(user_info: UserInfo):
     service_template_file = os.path.join(template_folder, "service-template.yml")
     ingress_template_file = os.path.join(template_folder, "ingress-template.yml")
     certificate_template_file = os.path.join(template_folder, "certificate-template.yml")
+    mysql_template_file = os.path.join(template_folder, "mysql-template.yaml")
+    mysqlservice_template_file = os.path.join(template_folder, "mysql-service-template.yaml")
     
     try:
         # Update issuer_template.yml
@@ -273,13 +332,67 @@ async def update_template_files(user_info: UserInfo):
         
         logger.info(f"Certificate template file '{certificate_template_file}' updated successfully")
 
+        # Update mysql-template.yml
+        with open(mysql_template_file, 'r') as file:
+            mysql_content = file.read()
+        
+        mysql_content = mysql_content.replace('{{ userID }}', str(user_info.userID))
+        
+        mysql_yaml_content = yaml.safe_load(mysql_content)
+        
+        with open(mysql_template_file, 'w') as file:
+            yaml.dump(mysql_yaml_content, file, default_flow_style=False)
+        
+        logger.info(f"Mysql template file '{mysql_template_file}' updated successfully")
+
+        # Update mysql-service-template.yml
+        with open(mysqlservice_template_file, 'r') as file:
+            mysqlservice_content = file.read()
+        
+        mysqlservice_content = mysqlservice_content.replace('{{ userID }}', str(user_info.userID))
+        
+        mysqlservice_yaml_content = yaml.safe_load(mysqlservice_content)
+        
+        with open(mysqlservice_template_file, 'w') as file:
+            yaml.dump(mysqlservice_yaml_content, file, default_flow_style=False)
+        
+        logger.info(f"Mysql-service template file '{mysqlservice_template_file}' updated successfully")
+
         return {"message": "Template files updated successfully"}
+
     except FileNotFoundError as e:
         logger.error(f"Template file not found: {str(e)}")
         raise HTTPException(status_code=404, detail=f"Template file not found: {str(e)}")
     except Exception as e:
         logger.error(f"Failed to update template files: {str(e)}")
         raise HTTPException(status_code=500, detail=f"Failed to update template files: {str(e)}")
+    
+async def create_k8s_objects(user_id: int):
+    kubeconfig = "kube-config"
+    base_path = f"users/user_{user_id}/k8s-template"
+    commands = [
+        f"kubectl apply -f {base_path}/namespace-template.yaml --kubeconfig={kubeconfig}",
+        f"kubectl apply -f {base_path}/configmap-template.yml --kubeconfig={kubeconfig}",
+        f"kubectl apply -f {base_path}/persistentvolumeclaim-template.yaml --kubeconfig={kubeconfig}",
+        f"kubectl apply -f {base_path}/issuer_template.yml --kubeconfig={kubeconfig}",
+        f"kubectl apply -f {base_path}/wordpress-template.yml --kubeconfig={kubeconfig}",
+        f"kubectl apply -f {base_path}/service-template.yml --kubeconfig={kubeconfig}",
+        f"kubectl apply -f {base_path}/certificate-template.yml --kubeconfig={kubeconfig}",
+        f"kubectl apply -f {base_path}/ingress-template.yml --kubeconfig={kubeconfig}"
+    ]
+    
+    results = []
+    for cmd in commands:
+        try:
+            result = subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
+            results.append(f"Command '{cmd}' executed successfully: {result.stdout}")
+        except subprocess.CalledProcessError as e:
+            error_message = f"Command '{cmd}' failed with error: {e.stderr}"
+            logger.error(error_message)
+            results.append(error_message)
+    
+    return results
+
 
 @app.post("/update_template_file/")
 async def update_template_file_endpoint(user_info: UserInfo):
@@ -312,6 +425,15 @@ async def update_template_files_endpoint(user_info: UserInfo):
 @app.post("/update_template_files/")
 async def update_template_files_endpoint(user_info: UserInfo):
     return await update_template_files(user_info)
+
+@app.post("/create_k8s_objects/")
+async def create_k8s_objects_endpoint(folder_info: FolderInfo):
+    try:
+        results = await create_k8s_objects(folder_info.userID)
+        return {"message": "Kubernetes objects created", "details": results}
+    except Exception as e:
+        logger.error(f"Failed to create Kubernetes objects: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Failed to create Kubernetes objects: {str(e)}")
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
